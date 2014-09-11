31f829ecb8e7e557c470c84c5535b4a6afaa5682
Christopher Wilson <element4emerica@gmail.com>
2014-09-11 16:40:08 -0400

    Query Refactoring

--- query/SortQueries.php
+++ query/SortQueries.php
@@ -6,92 +6,98 @@
  * Time: 12:53 PM
  */
 
-   class SortQueries {
+class SortQueries 
+{
 
-       private $sort_query;
+    private $sort_query;
 
-       public function __construct($sort_mode, $station_name) {
-           $this->setSortQuery($sort_mode, $station_name);
-       }
+    public function __construct($sort_mode, $station_name) 
+    {
+        $this->setSortQuery($sort_mode, $station_name);
+    }
 
-        /**
-        * @return mixed
-        */
-       public function getSortQuery()
-       {
-           return $this->sort_query;
-       }
+    /**
+    * @return mixed
+    */
+    public function getSortQuery()
+    {
+       return $this->sort_query;
+    }
 
-       /**
-        * calculate the start date;
-        * @param mixed $sort_query
-        */
-       public function setSortQuery($sort_mode, $station_name)
-       {
-           if ($station_name != null && $station_name != "all") {
-               $this->sort_query = "
+    /**
+    * calculate the start date;
+    * @param mixed $sort_query
+    */
+    public function setSortQuery($sort_mode, $station_name)
+    {
+        if ($station_name != null && $station_name != "all") 
+        {
+            $this->sort_query = "
+                SELECT  station.station_name, station_read.read_date
+                FROM    station_read
+                JOIN    station ON station.station_id = station_read.station_id
+                WHERE   station.station_name = '".$station_name."' AND
+                        station_read.read_date BETWEEN '".$_REQUEST['startdate']."' AND '".$_REQUEST['date']."'
+                ORDER BY station.station_name ASC, station_read.read_date DESC
+                LIMIT 0, 20
+            ";
+        } 
+
+        else 
+        {
+            switch ($sort_mode) 
+            {
+                case 'date' :
+                    $this->sort_query = "
                         SELECT  station.station_name, station_read.read_date
                         FROM    station_read
                         JOIN    station ON station.station_id = station_read.station_id
-                        WHERE   station.station_name = '".$station_name."' AND
-                                station_read.read_date BETWEEN '".$_REQUEST['startdate']."' AND '".$_REQUEST['date']."'
+                        ORDER BY station_read.read_date ASC
+                        LIMIT 0, 20
+                    ";
+                    break;
+
+                case 'month' :
+                    $this->sort_query = "
+                        SELECT  station.station_name, station_read.read_date
+                        FROM    station_read
+                        JOIN    station ON station.station_id = station_read.station_id
+                        ORDER BY MONTH(station_read.read_date) DESC
+                        LIMIT 0, 20
+                    ";
+                    break;
+
+                case 'day' :
+                    $this->sort_query = "
+                        SELECT  station.station_name, station_read.read_date
+                        FROM    station_read
+                        JOIN    station ON station.station_id = station_read.station_id
+                        ORDER BY DAY(station_read.read_date) DESC
+                        LIMIT 0, 20
+                    ";
+                    break;
+
+                case 'year' :
+                    $this->sort_query = "
+                        SELECT  station.station_name, station_read.read_date
+                        FROM    station_read
+                        JOIN    station ON station.station_id = station_read.station_id
+                        ORDER BY YEAR(station_read.read_date) DESC
+                        LIMIT 0, 20
+                    ";
+                    break;
+
+                default :
+                    $this->sort_query = "
+                        SELECT  station.station_name, station_read.read_date
+                        FROM    station_read
+                        JOIN    station ON station.station_id = station_read.station_id
+                        WHERE   station_read.read_date BETWEEN '".$_REQUEST['startdate']."' AND '".$_REQUEST['date']."'
                         ORDER BY station.station_name ASC, station_read.read_date DESC
                         LIMIT 0, 20
                     ";
-           } else {
-               switch ($sort_mode) {
-
-                   case 'date' :
-                       $this->sort_query = "
-                            SELECT  station.station_name, station_read.read_date
-                            FROM    station_read
-                            JOIN    station ON station.station_id = station_read.station_id
-                            ORDER BY station_read.read_date ASC
-                            LIMIT 0, 20
-                        ";
-                       break;
-
-                   case 'month' :
-                       $this->sort_query = "
-                            SELECT  station.station_name, station_read.read_date
-                            FROM    station_read
-                            JOIN    station ON station.station_id = station_read.station_id
-                            ORDER BY MONTH(station_read.read_date) DESC
-                            LIMIT 0, 20
-                        ";
-                       break;
-
-                   case 'day' :
-                       $this->sort_query = "
-                            SELECT  station.station_name, station_read.read_date
-                            FROM    station_read
-                            JOIN    station ON station.station_id = station_read.station_id
-                            ORDER BY DAY(station_read.read_date) DESC
-                            LIMIT 0, 20
-                        ";
-                       break;
-
-                   case 'year' :
-                       $this->sort_query = "
-                            SELECT  station.station_name, station_read.read_date
-                            FROM    station_read
-                            JOIN    station ON station.station_id = station_read.station_id
-                            ORDER BY YEAR(station_read.read_date) DESC
-                            LIMIT 0, 20
-                        ";
-                       break;
-
-                   default :
-                       $this->sort_query = "
-                            SELECT  station.station_name, station_read.read_date
-                            FROM    station_read
-                            JOIN    station ON station.station_id = station_read.station_id
-                            WHERE   station_read.read_date BETWEEN '".$_REQUEST['startdate']."' AND '".$_REQUEST['date']."'
-                            ORDER BY station.station_name ASC, station_read.read_date DESC
-                            LIMIT 0, 20
-                        ";
-                       break;
-               }
-           }
-       }
-   }
+                    break;
+            }
+        }
+    }
+}
--- query/change_password.php
+++ query/change_password.php
@@ -10,47 +10,63 @@
 
 $success = true;
 
-if (isset($_POST['user'])){
+if (isset($_POST['user'])
+{
     $user = $_POST['user'];
-} else {
+} 
+
+else 
+{
     $user = null;
     $success = false;
 }
 
-if (isset($_POST['password'])){
+if (isset($_POST['password']))
+{
     $password = $_POST['password'];
-} else {
+} 
+
+else 
+{
     $password = null;
     $success = false;
 }
 
-if (isset($_POST['new_password'])){
+if (isset($_POST['new_password']))
+{
     $new_password = $_POST['new_password'];
-} else {
+} 
+
+else 
+{
     $new_password = null;
     $success = false;
 }
 
-if (isset($_POST['confirm_password'])){
+if (isset($_POST['confirm_password']))
+{
     $confirm_password = $_POST['confirm_password'];
-} else {
+}
+
+else 
+{
     $confirm_password = null;
     $success = false;
 }
 
-if ($success) {
+if ($success) 
+{
     include_once('dbconnect.php');
     $query = "SELECT * FROM `station_users` WHERE user_name='".mysql_real_escape_string($user)."'";
     $result = mysql_query($query, $connection);
     echo $result;
     $data = mysql_fetch_array($result);
     $current_password = $data[2];
-    if ($current_password == md5($password) and $new_password == $confirm_password){
+
+    if ($current_password == md5($password) and $new_password == $confirm_password)
+    {
         $query="UPDATE station_users SET password='".md5($new_password)."' where user_name='".
             str_replace("'","''",$data[1])."'";
         mysql_query($query, $connection);
     }
 }
-
-
-
